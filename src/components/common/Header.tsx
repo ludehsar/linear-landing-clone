f91a17b22c44de40963bdfe592e2684299323848
Rashedul Alam <rashedulalam112112@gmail.com>
2023-06-18 13:14:07 +0600

    Implement animation in the navigation

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -18,41 +18,49 @@ export const Header = () => {
         <Link href="/">
           <Logo />
         </Link>
-        <nav
+        <div
           className={classNames(
-            "h-[calc(100vh_-_var(--navigation-height))] md:block fixed top-navigation-height left-0 w-full bg-background overflow-auto md:relative md:top-0 md:h-auto md:w-auto md:bg-transparent",
-            isHamburgerMenuOpen ? "" : "hidden"
+            "transition-[visibility] md:visible",
+            isHamburgerMenuOpen ? "visible" : "invisible delay-500"
           )}
         >
-          <ul
+          <nav
             className={classNames(
-              "flex flex-col md:flex-row md:items-center h-full [&_li]:ml-6 [&_li]:border-b [&_li]:border-dark-grey md:[&_li]:border-none",
-              "[&_a]:flex [&_a]:items-center [&_a]:h-navigation-height [&_a]:w-full [&_a]:text-md md:[&_a]:text-sm [&_a:hover]:text-grey [&_a]:transition-colors"
+              "transition-opacity duration-500 h-[calc(100vh_-_var(--navigation-height))] md:block fixed top-navigation-height left-0 w-full bg-background overflow-auto md:relative md:top-0 md:h-auto md:w-auto md:bg-transparent md:opacity-100",
+              isHamburgerMenuOpen ? "opacity-100" : "opacity-0"
             )}
           >
-            <li>
-              <Link href="#">Features</Link>
-            </li>
-            <li>
-              <Link href="#">Method</Link>
-            </li>
-            <li className="md:hidden lg:block">
-              <Link href="#">Customers</Link>
-            </li>
-            <li className="md:hidden lg:block">
-              <Link href="#">Changelog</Link>
-            </li>
-            <li className="md:hidden lg:block">
-              <Link href="#">Integrations</Link>
-            </li>
-            <li>
-              <Link href="#">Pricing</Link>
-            </li>
-            <li>
-              <Link href="#">Company</Link>
-            </li>
-          </ul>
-        </nav>
+            <ul
+              className={classNames(
+                "flex flex-col md:flex-row md:items-center h-full [&_li]:ml-6 [&_li]:border-b [&_li]:border-dark-grey md:[&_li]:border-none",
+                "[&_a]:flex [&_a]:items-center [&_a]:h-navigation-height [&_a]:w-full [&_a]:text-md md:[&_a]:text-sm [&_a:hover]:text-grey [&_a]:translate-y-8 [&_a]:transition-[transform,color] md:[&_a]:translate-y-0 [&_a]:duration-300 ease-in",
+                isHamburgerMenuOpen && "[&_a]:translate-y-0"
+              )}
+            >
+              <li>
+                <Link href="#">Features</Link>
+              </li>
+              <li>
+                <Link href="#">Method</Link>
+              </li>
+              <li className="md:hidden lg:block">
+                <Link href="#">Customers</Link>
+              </li>
+              <li className="md:hidden lg:block">
+                <Link href="#">Changelog</Link>
+              </li>
+              <li className="md:hidden lg:block">
+                <Link href="#">Integrations</Link>
+              </li>
+              <li>
+                <Link href="#">Pricing</Link>
+              </li>
+              <li>
+                <Link href="#">Company</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <div className="ml-auto h-full flex items-center">
           <Link className="text-sm mr-6" href="#">
             Login
